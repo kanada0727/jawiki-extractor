d0c5509bf8d400a2e0425171b48252ceae88337e
kanada0727 <kanada@pcl.cs.waseda.ac.jp>
2020-07-18 14:50:29 +0900

    feat: add simple page generator

---
+++ jawiki_extractor/raw_page_generator.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass, field
+from typing import List
+
+PAGE_HEADER = "  <page>\n"
+PAGE_FOOTER = "  </page>\n"
+
+
+@dataclass
+class RawPage:
+    lines: List[str] = field(default_factory=list)
+
+
+class RawPageGenerator:
+    def __init__(self, file_path):
+        self.line_iterator = open(file_path)
+
+    def __iter__(self):
+        state = "find_page"
+        lines = []
+        for line in self.line_iterator:
+
+            if state == "find_page":
+                if line == PAGE_HEADER:
+                    state = "append_line"
+
+            elif state == "append_line":
+                if line == PAGE_FOOTER:
+                    yield RawPage(lines)
+                    lines = []
+                    state = "find_page"
+                else:
+                    lines.append(line.lstrip())
