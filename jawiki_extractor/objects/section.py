369c57d536127f409a28ecdef6235256a85619d8
kanada0727 <kanada@pcl.cs.waseda.ac.jp>
2020-07-23 18:09:31 +0900

    refactor(Section): export constants

--- jawiki_extractor/objects/section.py
+++ jawiki_extractor/objects/section.py
@@ -1,14 +1,11 @@
-class Section:
-    IGNORE_HEADERS = {
-        "脚注",
-        "外部リンク",
-        "関連項目",
-        "出典",
-        "注釈",
-        "脚注",
-        "参考文献",
-    }
+import yaml
+import os
 
+_IGNORE_HEADERS_PATH = os.path.join(os.path.dirname(__file__), "../consts/ignore_section_headers.yml")
+IGNORE_HEADERS = set(yaml.safe_load(open(_IGNORE_HEADERS_PATH)))
+
+
+class Section:
     def __init__(self, header="", level=2):
         self.header = header
         self.texts = []
@@ -17,19 +14,11 @@ class Section:
         self.children = []
         self.is_acceptable = True
 
-    def __iter__(self):
-        yield self
-        for child in self.children:
-            yield from iter(child)
-
     def acceptable_iterator(self):
         if self.is_acceptable:
             yield self
             for child in self.children:
                 yield from child.acceptable_iterator()
-
-    def __repr__(self):
-        return f"Section(header={self.header}, children={self.children})"
 
     def set_header(self, line):
         char_count = 0
@@ -42,4 +31,7 @@ class Section:
         self.header = line[char_count:-char_count].strip()
 
     def validate(self):
-        self.is_acceptable = self.parent.is_acceptable and self.header not in self.IGNORE_HEADERS
+        self.is_acceptable = self.parent.is_acceptable and self.header not in IGNORE_HEADERS
+
+    def __repr__(self):
+        return f"Section(header={self.header}, children={self.children})"
