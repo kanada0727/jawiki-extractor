3c7151d7398858d79670db113e48f797061452c1
kanada0727 <kanada@pcl.cs.waseda.ac.jp>
2020-08-16 13:47:54 +0900

    feat: remain decoration line and add get text func

--- jawiki_extractor/objects/wiki_page.py
+++ jawiki_extractor/objects/wiki_page.py
@@ -7,5 +7,11 @@ class WikiPage:
         self.is_acceptable = True
         self.sections = Sections()
 
+    def get_all_texts(self):
+        lines = []
+        for section in self.sections:
+            lines.extend(section.texts)
+        return "\n".join(lines)
+
     def __repr__(self):
         return f"WikiPage(title={self.title} section={self.sections})"
--- jawiki_extractor/page_parser/structure_parser/line_verifier.py
+++ jawiki_extractor/page_parser/structure_parser/line_verifier.py
@@ -5,5 +5,4 @@ class LineVerifier:
 
     @classmethod
     def is_acceptable(cls, line):
-        return (not LineProcessor.has_prefix(line, "decorations")) \
-               and (not LineProcessor.equals(line, "horizontal_line"))
+        return not LineProcessor.equals(line, "horizontal_line")
